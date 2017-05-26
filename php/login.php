f842e7cb57982f79eb0d33bad6f22c03dd70c274
josenunorodrigues <josenunorodrigues@hotmail.com>
2017-05-26 15:43:59 +0100

    Login User

---
+++ php/login.php
@@ -0,0 +1,24 @@
+<?php
+    require_once( "../Lib/lib.php" );
+    
+    $linkIdentifier = mysqli_connect("localhost:3306", "root", "");      //TODO replace localhost with nameSpace
+    mysqli_select_db($linkIdentifier, "smi_final");
+    
+    $name = webAppName();
+    
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    $serverName = $_SERVER['SERVER_NAME'];
+
+    $serverPort = 80;
+    
+    $baseUrl = "http://" . $serverName . ":" . $serverPort;
+    $newbase="/mainPage.php";
+    $baseNextUrl = $baseUrl . $name . $newbase;
+
+    $id = isValid($_POST["username"], $_POST["password"]);
+
+    session_start();
+    $_SESSION["userId"] = $id;
+    header("location:mainPage.php");
+?>
