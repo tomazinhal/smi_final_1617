d28506860e9f81a66d97dff62ae2bef2269fba7f
josenunorodrigues <josenunorodrigues@hotmail.com>
2017-06-09 23:57:00 +0100

    If file when creating events is not an image

---
+++ php/eventFailed.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<title>SMI Final Project</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<body style="max-width:300px">
+    <div class="w3-modal"  style="display:block">
+        <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:300px">
+        <div class="w3-center"><br>
+            <a href="mainPage.php" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</a>
+        </div>
+
+        <p class="w3-button w3-margin-top">File must be an image!</p>
+        <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+            <a href="mainPage.php" type="button" class="w3-button w3-red">Go back</a>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
