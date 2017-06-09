74adf53dd5423075e8ab341be737ca6489f2ed7f
josenunorodrigues <josenunorodrigues@hotmail.com>
2017-06-09 23:53:07 +0100

    Code clean up

--- php/sendFile.php
+++ php/sendFile.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
- 
 <?php
     require_once( "../Lib/lib.php" );
     if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
