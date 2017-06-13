a834dc3ca278436f611a470031498c794ce176d9
josenunorodrigues <josenunorodrigues@hotmail.com>
2017-06-13 03:41:47 +0100

    Code cleanup

--- php/sendFile.php
+++ php/sendFile.php
@@ -1,15 +1,17 @@
 <?php
-    require_once( "../Lib/lib.php" );
     if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
         
-        $files = array();
-        foreach($_FILES as $key0=>$FILES) {
+        $files = array();                       //these loops is to reorder the $_FILES array (check at the end 
+        foreach($_FILES as $key0=>$FILES) {     //of the php file how the array is going too look like)
             foreach($FILES as $key=>$value) {
                 foreach($value as $key2=>$value2) {
                     $files[$key0][$key2][$key] = $value2;
                 }
             }
         }
+
+        $linkIdentifier = mysqli_connect("localhost:3306", "root", "");      //TODO replace localhost with nameSpace
+        mysqli_select_db($linkIdentifier, "smi_final");
 
         for ($i = 0; $i < count($files["content"]); $i++) {
             $content = $files["content"][$i];
@@ -19,8 +21,7 @@
             if($type == "image"){
                 ini_set('max_execution_time', 0);
 
-                $linkIdentifier = mysqli_connect("localhost:3306", "root", "");      //TODO replace localhost with nameSpace
-                mysqli_select_db($linkIdentifier, "smi_final");
+                
                 $query = "INSERT INTO `post`(`user_id`, `event_id`, `content`) VALUES (" . $_POST['userId'] . "," . $_POST['eventId'] . ", 'temp')";
                 mysqli_query($linkIdentifier, $query);
 
@@ -32,14 +33,11 @@
                 imagepng(imagecreatefromstring(file_get_contents($content["tmp_name"])), "../" . $directory);
                 $query = "UPDATE `post` SET `content`='" . $directory . "' WHERE `id` = " . $lastId;
                 mysqli_query($linkIdentifier, $query);
-                mysqli_close($linkIdentifier);
                 
             }
             if($type == "video"){
                 ini_set('max_execution_time', 0);
 
-                $linkIdentifier = mysqli_connect("localhost:3306", "root", "");      //TODO replace localhost with nameSpace
-                mysqli_select_db($linkIdentifier, "smi_final");
                 $query = "INSERT INTO `post`(`user_id`, `event_id`, `content`) VALUES (" . $_POST['userId'] . "," . $_POST['eventId'] . ", 'temp')";
                 mysqli_query($linkIdentifier, $query);
 
@@ -56,13 +54,14 @@
                 
                 $query = "UPDATE `post` SET `content`='" . $directory . "' WHERE `id` = " . $lastId;
                 mysqli_query($linkIdentifier, $query);
-                mysqli_close($linkIdentifier);
-                echo "<br>";
-                echo("comand: " . $comand . "\n<br>");
-                
             }
-            header("Location:eventPage.php?eventId=".$_POST["eventId"]);
         }
+
+        $query = "UPDATE `sub` SET `notification`= TRUE WHERE `event_id` = " . $_POST['eventId'];
+        mysqli_query($linkIdentifier, $query);
+
+        mysqli_close($linkIdentifier);
+        header("Location:eventPage.php?eventId=".$_POST["eventId"]);
     }
 ?>
 
@@ -72,7 +71,7 @@
 
 
 
-<!--    example of what the 3 foreach do (line 12-19)
+<!--    example of what the 3 foreach do
 Array 
 ( 
     [content] => Array 
