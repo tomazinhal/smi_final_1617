b4aa266dd28822ef209671fb9ea2e5157e49868e
josenunorodrigues <josenunorodrigues@hotmail.com>
2017-06-13 03:42:51 +0100

    Unsubscribes to an event

---
+++ php/unsubscribe.php
@@ -0,0 +1,13 @@
+<?php
+
+    session_start();
+    
+    $linkIdentifier = mysqli_connect("localhost:3306", "root", "");      //TODO replace localhost with nameSpace
+    mysqli_select_db($linkIdentifier, "smi_final");
+
+    $query = "DELETE FROM `sub` WHERE `event_id`= " . $_SESSION['eventId'] . " AND `user_id` = " . $_SESSION['userId'];
+    mysqli_query($linkIdentifier, $query);
+    
+    mysqli_close($linkIdentifier);
+    header("Location:eventPage.php?eventId=" . $_SESSION["eventId"]);
+?>
