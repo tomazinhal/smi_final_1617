934cc7b08fbce6f3bcfc6858a789cbb89a6cda72
josenunorodrigues <josenunorodrigues@hotmail.com>
2017-06-13 03:40:16 +0100

    Code cleanup

--- php/getTwelvePosts.php
+++ php/getTwelvePosts.php
@@ -1,15 +1,7 @@
 <?php
-    require_once( "../Lib/lib.php" );
     $linkIdentifier = mysqli_connect("localhost:3306", "root", "");      //TODO replace localhost with nameSpace
     mysqli_select_db($linkIdentifier, "smi_final");
-    
-    $name = webAppName();
-    
-    $method = $_SERVER['REQUEST_METHOD'];
 
-    $serverName = $_SERVER['SERVER_NAME'];
-
-    $serverPort = 80;
     $posts = array();
     if(isset($_POST["eventId"])){
 
