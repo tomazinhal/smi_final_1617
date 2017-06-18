0bf054461f5bef92da2ef025c4a771b200a2271f
tomazinhal <tomazinhal@gmail.com>
2017-06-18 22:41:31 +0100

    database update

---
+++ configs/insert_database.sql
@@ -0,0 +1,11 @@
+#user role
+INSERT INTO role (`id`, `name`) VALUES(0, "Guest");
+INSERT INTO role (`id`, `name`) VALUES(1, "User");
+INSERT INTO role (`id`, `name`) VALUES(2, "Supporter");
+INSERT INTO role (`id`, `name`) VALUES(3, "Administrator");
+
+#event type
+INSERT INTO type (`id`, `name`) VALUES(0, "Party");
+INSERT INTO type (`id`, `name`) VALUES(1, "Exhibition");
+INSERT INTO type (`id`, `name`) VALUES(2, "Art");
+INSERT INTO type (`id`, `name`) VALUES(3, "Festival");
