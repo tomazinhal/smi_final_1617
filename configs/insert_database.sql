85f9f030e9b242a24d3e966d4474a703652958a9
tomazinhal <tomazinhal@gmail.com>
2017-06-19 22:14:21 +0100

    insert data to database

--- configs/insert_database.sql
+++ configs/insert_database.sql
@@ -9,3 +9,8 @@ INSERT INTO type (`id`, `name`) VALUES(0, "Party");
 INSERT INTO type (`id`, `name`) VALUES(1, "Exhibition");
 INSERT INTO type (`id`, `name`) VALUES(2, "Art");
 INSERT INTO type (`id`, `name`) VALUES(3, "Festival");
+
+#insert users
+INSERT INTO user (`name`, `email`, `password`, `role`) VALUES("admin", "admin", "admin", 3)
+INSERT INTO user (`name`, `email`, `password`, `role`) VALUES("supporter", "supporter", "supporter", 2)
+
